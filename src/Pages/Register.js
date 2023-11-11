b26084432bda6f2de2be54babbec21b7d48391a5
NAVANEETH V <118598740+21ITR069@users.noreply.github.com>
2023-11-11 19:33:46 +0530

    Update Register.js

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -1,87 +1,117 @@
-import React, { useState } from 'react'
-import Navbar from '../Component/Navbar'
-import './LoginReg.css'
-import { Link, useNavigate } from 'react-router-dom'
-const Register = () => {
+import React, { useState } from 'react';
+import Navbar from '../Component/Navbar';
+import './LoginReg.css';
+import { Link, useNavigate } from 'react-router-dom';
 
-  
-    const empty = {
-      name: "",
-      email: "",
-      password: "",
-    };
-    const navigate = useNavigate();
-    const [singupInputs, setSignUpInputs] = useState(empty);
-    const handleClick = (event) => {
-      const { name, value } = event.target;
-      setSignUpInputs({ ...singupInputs, [name]: value });
-    };
-    // console.log(singupInputs);
-    const userDetails = async () => {
-      if (
-        singupInputs.name === "" ||
-        singupInputs.email === "" ||
-        singupInputs.password === ""
-      ) {
-        alert("please enter data in all fields");
-      } else {
+const Register = () => {
+  const empty = {
+    name: '',
+    email: '',
+    password: '',
+  };
+
+  const navigate = useNavigate();
+  const [singupInputs, setSignUpInputs] = useState(empty);
+  const [loading, setLoading] = useState(false);
+
+  const handleClick = (event) => {
+    const { name, value } = event.target;
+    setSignUpInputs({ ...singupInputs, [name]: value });
+  };
+
+  const userDetails = async () => {
+    if (
+      singupInputs.name === '' ||
+      singupInputs.email === '' ||
+      singupInputs.password === ''
+    ) {
+      alert('Please enter data in all fields');
+    } else {
+      setLoading(true);
+      try {
         let record = await fetch("https://gymspark.onrender.com/register", {
-          method: "POST",
-          headers: { "Content-Type": "application/json" },
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
           body: JSON.stringify(singupInputs),
         });
-        record= await record.json();
-        if (record ==="email already exists") {
-          alert("email already exists!"); 
+        record = await record.json();
+        if (record === 'email already exists') {
+          alert('Email already exists!');
         } else {
           setSignUpInputs(empty);
-          alert("Signed Up Successfully!");
-          navigate("/login");
+          alert('Signed Up Successfully!');
+          navigate('/login');
         }
+      } catch (error) {
+        console.error('Error during registration:', error);
+      } finally {
+        setLoading(false);
       }
-    };
-
+    }
+  };
 
   return (
- 
     <>
-       <>
       <Navbar />
 
-      <div className='bg-img'>
-            <div className="login-box mt-5">
-              <p>Sign Up</p>
-              <form onSubmit={(e) => e.preventDefault()}>
-              <div className="user-box">
-                  <input required name="name" type="text" onChange={handleClick} value={singupInputs.name}/>
-                  <label>Name</label>
-                </div>
-                <div className="user-box">
-                  <input required name="email" type="email" onChange={handleClick} value={singupInputs.email} />
-                  <label>Email</label>
-                </div>
-                <div className="user-box">
-                  <input required name="password" type="password" onChange={handleClick} value={singupInputs.password} />
-                  <label>Password</label>
-                </div>
-                <Link onClick={userDetails} >
-                  <span />
-                  <span />
-                  <span />
-                  Submit
-                </Link>
-              </form>
-              <p>Do you have an account? <Link to='/login' className="a2">Login</Link></p>
+      <div className="bg-img">
+        <div className="login-box mt-5">
+          <p>Sign Up</p>
+          <form onSubmit={(e) => e.preventDefault()}>
+            <div className="user-box">
+              <input
+                required
+                name="name"
+                type="text"
+                onChange={handleClick}
+                value={singupInputs.name}
+              />
+              <label>Name</label>
             </div>
+            <div className="user-box">
+              <input
+                required
+                name="email"
+                type="email"
+                onChange={handleClick}
+                value={singupInputs.email}
+              />
+              <label>Email</label>
+            </div>
+            <div className="user-box">
+              <input
+                required
+                name="password"
+                type="password"
+                onChange={handleClick}
+                value={singupInputs.password}
+              />
+              <label>Password</label>
+            </div>
+            <Link onClick={userDetails}>
+              <span />
+              <span />
+              <span />
+              Submit
+            </Link>
+          </form>
+          <p>
+            Do you have an account?{' '}
+            <Link to="/login" className="a2">
+              Login
+            </Link>
+          </p>
+        </div>
       </div>
 
-
-
-
+      {loading && (
+        <div className="loading-popup">
+          {/* Add your loading indicator or message here */}
+          Loading...
+        </div>
+      )}
     </>
-    
-    </>
-  )
-}
+  );
+};
 
 export default Register;
